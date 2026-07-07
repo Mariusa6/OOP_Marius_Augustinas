416794a35ca5fdb10653ffcfb8e4537542bcc13c
Mariusa6 <marius.augustinas@mif.stud.vu.lt>
2026-07-07 09:46:18 +0300

    v0.4 8 - tyrimo papildymas

--- test.cpp
+++ test.cpp
@@ -16,8 +16,15 @@ void testData(int n)
 {
     std::vector<studentas> studentai {};
     auto start = std::chrono::high_resolution_clock::now();
-    readStudentaiFromFile("data/studentai" + std::to_string(n) + ".txt");
+    studentai = readStudentaiFromFile("data/studentai" + std::to_string(n) + ".txt");
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
     std::cout << n << " Studentų failo skaitymas užtruko: " << elapsed.count() << " sekundžių.\n";
+    calculateGalutinisVector(studentai, namuDarbaiSvertis, egzaminasSvertis);
+    sortStudentai(studentai, '4');
+    auto start2 = std::chrono::high_resolution_clock::now();
+    splitResult result = splitStudentai(studentai);
+    auto end2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed2 = end2 - start2;
+    std::cout << n << " Studentų skirstymas į kietiakai ir vargsiukai užtruko: " << elapsed2.count() << " sekundžių.\n";
 }
