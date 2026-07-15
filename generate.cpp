4696bab073b1a4213962b54cd855fc84c57d2d29
Mariusa6 <marius.augustinas@mif.stud.vu.lt>
2026-07-15 16:19:37 +0300

    v1.0 11

--- generate.cpp
+++ generate.cpp
@@ -6,40 +6,6 @@ std::mt19937& getRng()
     return rng;
 }
 
-std::vector<studentas> generateStudentai(int n)
-{
-    std::vector<studentas> studentai;
-    studentai.reserve(n);
-    for (int i = 0; i < n; i++)
-    {
-        studentas s;
-        s.vardas = "VardasNR" + std::to_string(i + 1);
-        s.pavarde = "PavardeNR" + std::to_string(i + 1);
-        int m = NUMBER_OF_PAZYMYS;
-        s.namuDarbai = generatePazymiai(m);
-        s.egzaminas = generatePazymiai(1)[0];
-        studentai.push_back(s);
-    }
-    return studentai;
-}
-
-std::vector<studentas> generateOnlyPazymiai(int n)
-{
-    std::vector<studentas> studentai;
-    studentai.reserve(n);
-    for (int i = 0; i < n; i++)
-    {
-        studentas s;
-        s.vardas = enterName(i + 1);
-        s.pavarde = enterSurname(i + 1);
-        int m = NUMBER_OF_PAZYMYS;
-        s.namuDarbai = generatePazymiai(m);
-        s.egzaminas = generatePazymiai(1)[0];
-        studentai.push_back(s);
-    }
-    return studentai;
-}
-
 std::vector<int> generatePazymiai(int m) // m - pažymių skaičius
 {
     std::vector<int> tmp;
--- generate.h
+++ generate.h
@@ -6,4 +6,48 @@
 #include <string>   // string
 #include <random>   // mt19937, random_device
 
+// -------------------------------------------------------
+// Template: generateStudentai
+// Generuoja studentus su atsitiktiniais vardais, pavardėmis ir pažymiais.
+// -------------------------------------------------------
+template<typename Container>
+Container generateStudentai(int n)
+{
+    Container studentai;
+    studentai.reserve(n);
+    for (int i = 0; i < n; i++)
+    {
+        studentas s;
+        s.vardas = "VardasNR" + std::to_string(i + 1);
+        s.pavarde = "PavardeNR" + std::to_string(i + 1);
+        int m = NUMBER_OF_PAZYMYS;
+        s.namuDarbai = generatePazymiai(m);
+        s.egzaminas = generatePazymiai(1)[0];
+        studentai.push_back(s);
+    }
+    return studentai;
+}
+
+// -------------------------------------------------------
+// Template: generateOnlyPazymiai
+// Generuoja tik atsitiktinius pažymius studentams.
+// -------------------------------------------------------
+template<typename Container>
+Container generateOnlyPazymiai(int n)
+{
+    Container studentai;
+    studentai.reserve(n);
+    studentas s;
+    for (int i = 0; i < n; i++)
+    {
+        s.vardas = enterName(i + 1);
+        s.pavarde = enterSurname(i + 1);
+        int m = NUMBER_OF_PAZYMYS;
+        s.namuDarbai = generatePazymiai(m);
+        s.egzaminas = generatePazymiai(1)[0];
+        studentai.push_back(s);
+    }
+    return studentai;
+}
+
 #endif // GENERATE_H_DEFINED
